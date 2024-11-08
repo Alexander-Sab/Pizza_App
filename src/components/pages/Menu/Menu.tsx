381e9fd7bd00c28718cdb1a84d3050cca6178316
Alexander-Sab <113023875+Alexander-Sab@users.noreply.github.com>
2024-11-08 16:05:52 +0300

    сообщение коммита

--- src/components/pages/Menu/Menu.tsx
+++ src/components/pages/Menu/Menu.tsx
@@ -1,5 +1,6 @@
 import { Search } from '../../../Search/Search';
 import { Headling } from '../../Headling/Headling';
+import { ProductCard } from '../../ProductCard/ProductCard';
 import styles from './Menu.module.css';
 
 export function Menu() {
@@ -8,6 +9,16 @@ export function Menu() {
 			<div className={styles.head}>
 				<Headling>Меню</Headling>
 				<Search placeholder="Введите блюдо или состав" />
+			</div>
+			<div>
+				<ProductCard
+					id={1}
+					title="Наслаждение"
+					description="Салями, руккола, помидоры, оливки"
+					image="/product-demo.png"
+					price={300}
+					rating={4.5}
+				/>
 			</div>
 		</>
 	);
